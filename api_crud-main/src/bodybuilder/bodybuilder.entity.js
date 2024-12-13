c7e843087c8ce9b6ba1da472ff97572afafdcd67
Lara <pedrohenrique.goncalves753@gmail.com>
2024-12-12 22:14:58 -0300

    iu

---
+++ api_crud-main/src/bodybuilder/bodybuilder.entity.js
@@ -0,0 +1,13 @@
+class BodyBuilder{
+    constructor(cpf, nome, peso, altura, dataNascimento, sapato, gym, style){
+        this.cpf = cpf
+        this.nome = nome
+        this.peso = peso
+        this.altura = altura
+        this.dataNascimento = dataNascimento
+        this.sapato = sapato
+        this.gym = gym
+        this.style = style
+    }
+}
+module.exports = BodyBuilder
---
+++ api_crud-main/src/estilo/style.entity.js
@@ -0,0 +1,8 @@
+class Style {
+    constructor(id, nome) {
+        this.id = id
+        this.nome = nome
+    }
+}
+
+module.exports = Style;
